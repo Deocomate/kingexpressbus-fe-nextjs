87a13110418c6ed30b2d1201b54961bc7af28faf
minhlong <deocomate@gmail.com>
2026-08-08 15:31:25 +0700

    update: package-lock.json, layout.jsx, package.json

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 export const metadata = {
   title: "King Express Bus",
@@ -22,6 +23,7 @@ export default async function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
